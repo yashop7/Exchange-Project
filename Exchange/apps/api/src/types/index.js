4ede99723d8a4143a227fadf6f08cdd1147ebef1
Yash <139779840+yashop7@users.noreply.github.com>
2025-01-22 23:53:42 +0530

    Remove unused type definitions for cleaner codebase

--- Exchange/apps/api/src/types/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GET_DEPTH = exports.ON_RAMP = exports.CANCEL_ORDER = exports.CREATE_ORDER = void 0;
-exports.CREATE_ORDER = "CREATE_ORDER";
-exports.CANCEL_ORDER = "CANCEL_ORDER";
-exports.ON_RAMP = "ON_RAMP";
-exports.GET_DEPTH = "GET_DEPTH";
